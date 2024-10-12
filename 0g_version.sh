c901cd428a2b94c31ad063e99755d15f6c5797c2
Zstake <hyunjubd@gmail.com>
2024-10-13 05:52:24 +0900

    Update 0g_version.sh

--- 0g_version.sh
+++ 0g_version.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# ANSI escape code for yellow text
+YELLOW='\033
+
 echo "Checking version of 0gchain..."
 cd $HOME/0gchaind
 0gchaind version
@@ -7,19 +10,24 @@ cd $HOME/0gchaind
 echo "Checking version for storage node..."
 cd $HOME/0g-storage-node
 git log --decorate=short --oneline | grep "tag: v" | head -n 1
+cd
 
 echo "Checking version for storage client..."
 cd $HOME/0g-storage-client
 git log --decorate=short --oneline | grep "tag: v" | head -n 1
+cd
 
 echo "Checking version for da node..."
 cd $HOME/0g-da-node
 git log --decorate=short --oneline | grep "tag: v" | head -n 1
+cd
 
 echo "Checking version for da client..."
 cd $HOME/0g-da-client
 git log --decorate=short --oneline | grep "tag: v" | head -n 1
+cd
 
 echo "Checking version for storage kv..."
 cd $HOME/0g-storage-kv
 git log --decorate=short --oneline | grep "tag: v" | head -n 1
+cd
