11fd5391632d10b23d5a4a1e737d05318f04daf5
Zstake <hyunjubd@gmail.com>
2024-10-13 06:00:43 +0900

    Update 0g_version.sh

--- 0g_version.sh
+++ 0g_version.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
-
-
+echo "Checking version of 0gchaind..."
 cd $HOME/0gchaind
 0gchaind version
 
-echo "Checking version for storage node..."
+echo "Checking tag version for 0g-storage-node..."
 cd $HOME/0g-storage-node
 git log --decorate=short --oneline | grep "tag: v" | head -n 1
-cd
 
-echo "Checking version for storage client..."
+echo "Checking tag version for 0g-storage-client..."
 cd $HOME/0g-storage-client
 git log --decorate=short --oneline | grep "tag: v" | head -n 1
-cd
 
-echo "Checking version for da node..."
+echo "Checking tag version for 0g-da-node..."
 cd $HOME/0g-da-node
 git log --decorate=short --oneline | grep "tag: v" | head -n 1
-cd
 
-echo "Checking version for da client..."
+echo "Checking tag version for 0g-da-client..."
 cd $HOME/0g-da-client
 git log --decorate=short --oneline | grep "tag: v" | head -n 1
-cd
 
-echo "Checking version for storage kv..."
+echo "Checking tag version for 0g-storage-kv..."
 cd $HOME/0g-storage-kv
 git log --decorate=short --oneline | grep "tag: v" | head -n 1
-cd
