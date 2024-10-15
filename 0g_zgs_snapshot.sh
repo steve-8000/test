0fdd9fb51ba05bea6606cb6cd885c3622ae7c1e7
Zstake <hyunjubd@gmail.com>
2024-10-16 06:23:18 +0900

    Create 0g_zgs_snapshot.sh

---
+++ 0g_zgs_snapshot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install necessary packages
+echo "Installing necessary packages..."
+sudo apt-get install wget lz4 aria2 pv -y
+
+# Download the snapshot with aria2
+echo "Downloading the snapshot..."
+aria2c -x 16 -s 16 https://snapshot.zstake.xyz/downloads/storage_0gchain_snapshot.lz4
+
+# Stop the zgs service
+echo "Stopping the zgs service..."
+sudo systemctl stop zgs
+
+# Remove old data
+echo "Removing old data..."
+rm -r $HOME/0g-storage-node/run/db
+rm -r $HOME/0g-storage-node/run/log
+rm -r $HOME/0g-storage-node/run/network
+
+# Extract the new snapshot
+echo "Extracting the new snapshot..."
+lz4 -c -d storage_0gchain_snapshot.lz4 | pv | tar -x -C $HOME/0g-storage-node/run
+
+echo "Process completed successfully!"
