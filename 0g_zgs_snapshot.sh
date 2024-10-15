3c4900c73f81616ec9896d986d7d835be58fa2d6
Zstake <hyunjubd@gmail.com>
2024-10-16 07:35:45 +0900

    Update 0g_zgs_snapshot.sh

--- 0g_zgs_snapshot.sh
+++ 0g_zgs_snapshot.sh
@@ -2,8 +2,7 @@
 
 # Install necessary packages
 echo "Installing necessary packages..."
-sudo apt-get install wget lz4 aria2 pv -y
-
+cd && rm -rf $HOME/storage_0gchain_snapshot.lz4 && sudo apt-get install wget lz4 aria2 pv -y
 # Download the snapshot with aria2
 echo "Downloading the snapshot..."
 aria2c -x 16 -s 16 https://snapshot.zstake.xyz/downloads/storage_0gchain_snapshot.lz4
