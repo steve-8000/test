284a99ec49e864fcb3ac48db07b93547e5cd3a5d
Zstake <hyunjubd@gmail.com>
2024-11-04 19:34:32 +0900

    Update 0g_storage_installer.sh

--- 0g_storage_installer.sh
+++ 0g_storage_installer.sh
@@ -23,7 +23,7 @@ echo "Fetching all tags..."
 git fetch --all --tags
 
 echo "Checking out specific commit..."
-git checkout 9eea71e
+git checkout 0711851
 
 echo "Updating submodules..."
 git submodule update --init
