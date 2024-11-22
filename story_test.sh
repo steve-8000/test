ba1571639ec9efc3271692800390817a936869d6
Zstake <hyunjubd@gmail.com>
2024-11-22 17:36:35 +0900

    Update story_test.sh

--- story_test.sh
+++ story_test.sh
@@ -15,7 +15,7 @@ while true; do
     blocks_left=0
   fi
 
-  echo -e "\033[1;33mNode Height:\033[1;34m $local_height\033[0m \033[1;33m| Network Height:\033[1;36m $network_height\033[0m \033[1;33m| Blocks Left:\033[1;31m $blocks_left\033[0m"
+  echo -e "\033[1;33m당신의 노드 블럭 높이:\033[1;34m $local_height\033[0m \033[1;33m| 네트워크 블럭 높이:\033[1;36m $network_height\033[0m \033[1;33m| 동기화 남은 블럭수:\033[1;31m $blocks_left\033[0m"
 
   sleep 5
 done
