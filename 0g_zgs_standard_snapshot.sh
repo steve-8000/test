b5cb6713e0032bcf361de63c86da7f820fbf45da
Zstake <hyunjubd@gmail.com>
2024-12-12 01:13:52 +0900

    Update 0g_zgs_standard_snapshot.sh

--- 0g_zgs_standard_snapshot.sh
+++ 0g_zgs_standard_snapshot.sh
@@ -3,9 +3,30 @@
 # Install necessary packages
 echo "Installing necessary packages..."
 cd && rm -rf $HOME/storage_0gchain_snapshot.lz4 && sudo apt-get install wget lz4 aria2 pv -y
-# Download the snapshot with aria2
+
+# Prompt user for snapshot selection
+echo "Please select the snapshot to download:"
+echo "1) Server 1 : Standard Storage Contract Snapshot"
+echo "2) Server 2 : Standard Storage Contract Snapshot"
+read -p "Enter the number (1 or 2): " snapshot_choice
+
+# Set the URL based on user input
+case $snapshot_choice in
+  1)
+    snapshot_url="http://snapshot_2.zstake.xyz/downloads/storage_0gchain_snapshot.lz4"
+    ;;
+  2)
+    snapshot_url="http://snapshot_3.zstake.xyz/downloads/storage_0gchain_snapshot.lz4"
+    ;;
+  *)
+    echo "Invalid choice. Exiting."
+    exit 1
+    ;;
+esac
+
+# Download the selected snapshot with aria2
 echo "Downloading the snapshot..."
-aria2c -x 16 -s 16 http://snapshot_2.zstake.xyz/downloads/storage_0gchain_snapshot.lz4
+aria2c -x 16 -s 16 $snapshot_url
 
 # Stop the zgs service
 echo "Stopping the zgs service..."
@@ -19,6 +40,4 @@ rm -r $HOME/0g-storage-node/run/network
 
 # Extract the new snapshot
 echo "Extracting the new snapshot..."
-lz4 -c -d storage_0gchain_snapshot.lz4 | pv | tar -x -C $HOME/0g-storage-node/run
-
-echo "Process completed successfully!"
+lz4 -c -d storage_0gchain_snapshot
