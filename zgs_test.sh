d49e507856ba0c124c2610582e389206d3703fb6
Zstake <hyunjubd@gmail.com>
2025-03-24 08:24:36 +0900

    Update zgs_test.sh

--- zgs_test.sh
+++ zgs_test.sh
@@ -3,7 +3,7 @@ while true; do
     logSyncHeight=$(echo $response | jq '.result.logSyncHeight')
     connectedPeers=$(echo $response | jq '.result.connectedPeers')
     network_height=$(curl -s https://og-testnet-rpc.itrocket.net/status | jq -r '.result.sync_info.latest_block_height')
-    Block_Left=$((network_height - logSyncHeight))
+    Deff=$((network_height - logSyncHeight))
     echo -e "Network Height: \033[33m$network_height\033[0m, Block: \033[32m$logSyncHeight\033[0m, Diff: \033[31m$Block_Left\033[0m, Peers: \033[34m$connectedPeers\033[0m"
     sleep 5; 
 done
