1aa8c54abd5551e7ee72947da0d1049dc322b21f
steve.kim <steve.kim@a41.io>
2025-10-16 11:21:53 +0900

    Update network_test.sh

--- network_test.sh
+++ network_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -euo pipefail
 N=5
 OUT=results.json
-: > "$OUT"
 
+: > "$OUT"
 echo "Running $N speedtests..."
 for i in $(seq 1 $N); do
   echo "[$i/$N] running..."
@@ -13,23 +14,23 @@ done
 
 echo "### 평균/표준편차 ###"
 jq -s '
-  def mean(stream): ( [stream] | add / length );
-  def variance(stream):
-    ( [stream] ) as $a
-    | ($a | add / length) as $m
-    | ( [ foreach $a[] as $x (0; .; .), ((($x - $m) * ($x - $m))) ] | add / length );
-  def stddev(stream): (variance(stream)) | sqrt;
+  def mean(a): (a|add) / (a|length);
+  def stddev(a):
+    (a|length) as $n
+    | (a|add) as $sum
+    | (a|map(. * .) | add) as $sum2
+    | (($sum2 / $n) - (($sum / $n) * ($sum / $n))) | sqrt;
 
-  ( [ .[] | select(.ping!=null)      | .ping ] )              as $pings
-| ( [ .[] | select(.download!=null) | (.download/1000000) ] ) as $downs
-| ( [ .[] | select(.upload!=null)   | (.upload/1000000) ] )   as $ups
+  ( [ .[] | select(.ping!=null)       | .ping ] )               as $pings
+| ( [ .[] | select(.download!=null)   | (.download/1000000) ] ) as $downs
+| ( [ .[] | select(.upload!=null)     | (.upload/1000000) ] )   as $ups
 | {
-    count:        ($pings|length),
-    avg_ping_ms:  ($pings | add / length),
-    std_ping_ms:  (stddev($pings[])),
-    avg_down_mbps:($downs | add / length),
-    std_down_mbps:(stddev($downs[])),
-    avg_up_mbps:  ($ups   | add / length),
-    std_up_mbps:  (stddev($ups[]))
+    count:           ($pings|length),
+    avg_ping_ms:     (mean($pings)),
+    std_ping_ms:     (stddev($pings)),
+    avg_down_mbps:   (mean($downs)),
+    std_down_mbps:   (stddev($downs)),
+    avg_up_mbps:     (mean($ups)),
+    std_up_mbps:     (stddev($ups))
   }
 ' "$OUT"
